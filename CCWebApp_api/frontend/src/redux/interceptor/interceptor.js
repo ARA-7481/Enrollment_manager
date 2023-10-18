4b2b682b6b6a01cf3c722f69a92d025a5539a2da
adler <agapay.adril@gmail.com>
2023-10-18 15:36:05 +0800

    errorhandlingupdate1

--- CCWebApp_api/frontend/src/redux/interceptor/interceptor.js
+++ CCWebApp_api/frontend/src/redux/interceptor/interceptor.js
@@ -21,7 +21,7 @@ instanceAxios.interceptors.response.use(
   response => response,
   async error => {
     const originalRequest = error.config;
-    if (error.response.status === 401 && !originalRequest._retry) {
+    if (error.response && error.response.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
 
       try {
