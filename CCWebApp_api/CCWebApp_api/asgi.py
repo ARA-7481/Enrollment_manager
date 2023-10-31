e5bfc845c3b19b05de9fa1e8483406cf2d28cbdf
adler <agapay.adril@gmail.com>
2023-10-31 14:21:33 +0800

    authmiddleware

--- CCWebApp_api/CCWebApp_api/asgi.py
+++ CCWebApp_api/CCWebApp_api/asgi.py
@@ -8,22 +8,18 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from main.middleware import TokenAuthMiddleware
+from channels.auth import AuthMiddlewareStack
+import main.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CCWebApp_api.settings')
 
-def get_websocket_application():
-    from main.middleware import TokenAuthMiddleware
-    from channels.routing import URLRouter
-    import main.routing
-
-    return TokenAuthMiddleware(
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
         URLRouter(
             main.routing.websocket_urlpatterns
         )
-    )
-
-application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
-    'websocket': get_websocket_application(),
+    ),
 })
