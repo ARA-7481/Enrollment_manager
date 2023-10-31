65797f4d1d21ac19e0a4aa8ac8824718291ee303
adler <agapay.adril@gmail.com>
2023-10-31 15:37:57 +0800

    withtwister

--- CCWebApp_api/CCWebApp_api/asgi.py
+++ CCWebApp_api/CCWebApp_api/asgi.py
@@ -12,10 +12,12 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import main.routing
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CCWebApp_api.settings')
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    "http": django_asgi_app,
     'websocket': AuthMiddlewareStack(
         URLRouter(
             main.routing.websocket_urlpatterns
